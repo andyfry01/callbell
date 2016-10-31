20d2555a50f8020bae9d7592dd6493d068dee278
Andy Fry <andyfry01@gmail.com>
2016-10-31 13:32:29 -0400

    grabbing buttons from dashboard page and logging

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -1,4 +1,14 @@
 window.onload = function() {
   console.log("hello from dashboard.js");
 
+  var editProfileButton = document.getElementById('edit_profile')
+  var viewProfileButton = document.getElementById('view_profile')
+  var browseProfilesButton = document.getElementById('browse_profiles')
+  var viewLikesButton = document.getElementById('view_likes')
+  var messagesButton = document.getElementById('messages')
+  var paymentsButton = document.getElementById('payments')
+  var contactButton = document.getElementById('contact')
+
+  console.log('buttons:');
+  console.log(editProfileButton, viewProfileButton, browseProfilesButton, viewLikesButton, messagesButton, paymentsButton, contactButton);
 }
