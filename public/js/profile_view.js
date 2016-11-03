8e58045c711a7fc01b65bfb27f4cce2826329db4
Andy Fry <andyfry01@gmail.com>
2016-11-03 16:56:45 -0400

    profile rendering in Hbars template

--- public/js/profile_view.js
+++ public/js/profile_view.js
@@ -1,28 +1,41 @@
-window.onload = function() {
-  console.log('hello from profile_view.js');
+var convertToJSON = function(object) {
+  return JSON.stringify(object)
+}
 
-  var sessionData =  {
+var getSessionStorage = function() {
+  var sessionInfo = {
     token: sessionStorage.getItem('token')
   }
+  return sessionInfo
+}
 
-  var fillTemplate = function(data) {
-    var source = document.getElementById('nurse_profile_template').innerHTML
-    var template = Handlebars.compile(source)
-    var html = template(data)
-    var targetDiv = document.getElementById('profile-container')
-    targetDiv.innerHTML = html
+var fillTemplate = function(data) {
+  var source = document.getElementById('nurse_profile_template').innerHTML
+  var template = Handlebars.compile(source)
+  var html = template(data)
+  var targetDiv = document.getElementById('profile-container')
+  targetDiv.innerHTML = html
+}
+
+var requestProfile = function(){
+  var data = {
+    sessionData: getSessionStorage()
   }
-
+  var dataJSON = convertToJSON(data)
   $.ajax({
     type: 'POST',
-    data: { sessionData },
+    data: { data: dataJSON },
     crossDomain: true,
     url: 'http://localhost:3000/user/profile',
     success: function(data) {
-      console.log('Data returned from API:');
-      console.log(data);
       fillTemplate(data)
     }
   })
+}
 
+window.onload = function() {
+
+  console.log('hello from profile_view.js');
+
+  requestProfile()
 }
